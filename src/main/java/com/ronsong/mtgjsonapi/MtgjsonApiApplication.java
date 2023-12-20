4d24e89e1121e737fbee232558bcbb2882e7bfd5
Ronnie Song <ronniesong0809@gmail.com>
2023-12-20 01:26:17 -0800

    init

---
+++ src/main/java/com/ronsong/mtgjsonapi/MtgjsonApiApplication.java
@@ -0,0 +1,13 @@
+package com.ronsong.mtgjsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgjsonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgjsonApiApplication.class, args);
+	}
+
+}
